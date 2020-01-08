cb7758210cea09bd443f6e1855ea280adcf76b8d
zhaocong <1140253608@qq.com>
2020-01-08 14:56:53 +0800

    修改:配置tags model

--- db.sql
+++ db.sql
@@ -1,11 +1,11 @@
 CREATE TABLE `blog_tag` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(100) DEFAULT '' COMMENT '标签名称',
-  `created_on` int(10) unsigned DEFAULT '0' COMMENT '创建时间',
+  `created_at` int(10) unsigned DEFAULT '0' COMMENT '创建时间',
   `created_by` varchar(100) DEFAULT '' COMMENT '创建人',
-  `modified_on` int(10) unsigned DEFAULT '0' COMMENT '修改时间',
-  `modified_by` varchar(100) DEFAULT '' COMMENT '修改人',
-  `deleted_on` int(10) unsigned DEFAULT '0',
+  `updated_at` int(10) unsigned DEFAULT '0' COMMENT '修改时间',
+  `updated_by` varchar(100) DEFAULT '' COMMENT '修改人',
+  `deleted_at` int(10) unsigned DEFAULT '0',
   `state` tinyint(3) unsigned DEFAULT '1' COMMENT '状态 0为禁用、1为启用',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='文章标签管理';
@@ -16,11 +16,11 @@ CREATE TABLE `blog_article` (
   `title` varchar(100) DEFAULT '' COMMENT '文章标题',
   `desc` varchar(255) DEFAULT '' COMMENT '简述',
   `content` text,
-  `created_on` int(11) DEFAULT NULL,
+  `created_at` int(11) DEFAULT NULL,
   `created_by` varchar(100) DEFAULT '' COMMENT '创建人',
-  `modified_on` int(10) unsigned DEFAULT '0' COMMENT '修改时间',
-  `modified_by` varchar(255) DEFAULT '' COMMENT '修改人',
-  `deleted_on` int(10) unsigned DEFAULT '0',
+  `updated_at` int(10) unsigned DEFAULT '0' COMMENT '修改时间',
+  `updated_by` varchar(255) DEFAULT '' COMMENT '修改人',
+  `deleted_at` int(10) unsigned DEFAULT '0',
   `state` tinyint(3) unsigned DEFAULT '1' COMMENT '状态 0为禁用1为启用',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='文章管理';
---
+++ models/tags.go
@@ -0,0 +1,32 @@
+package models
+
+import "github.com/jinzhu/gorm"
+
+//模型结构体
+type Tag struct {
+	gorm.Model
+
+	Name       string `json:"name"`
+	CreatedBy  string `json:"created_by"`
+	ModifiedBy string `json:"modified_by"`
+	State      int    `json:"state"`
+}
+
+//根据条件获取tags列表 返回数据指针
+func GetTags(pageNum int, pageSize int, maps interface{}) (tags *[]Tag) {
+	db.Where(maps).Offset(pageNum).Limit(pageSize).Find(&tags)
+
+	return
+}
+
+//获取tag总数量
+func GetTagTotalNum(maps interface{}) (count *int) {
+	db.Model(&Tag{}).Where(maps).Count(&count)
+
+	return
+}
+
+//设置表明
+func (Tag) TableName() string {
+	return "blog_tag"
+}
